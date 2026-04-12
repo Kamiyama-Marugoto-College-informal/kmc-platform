70ea3d7dd21a877650a883ee57a3f354da0d4813
Kizuki Aiki <aikikizuki@gmail.com>
2026-04-12 15:26:37 +0900

    feat(ui): Add development mode indicator bar
    `DevBar` component displays a fixed banner at the top of the screen when
    running in development mode (`import.meta.env.DEV`), helping developers
    distinguish between production and development environments at a glance.

--- src/main.tsx
+++ src/main.tsx
@@ -4,10 +4,12 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import { ThemeProvider } from '@/components/theme-provider'
 import { Root } from '@/Root'
+import DevBar from '@/components/DevBar'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
+      <DevBar />
       <Root />
     </ThemeProvider>
   </StrictMode>,
